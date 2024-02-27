261b87c9065a630162318513e819ff27bbbbeea5
resyahrial <syahrialrangga11@gmail.com>
2024-02-27 10:52:37 +0700

    create ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Button} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+      <Container className="pt-5 mb-4">
+        <h1 className='text-center mb-3'>ID Generator</h1>
+        <h3 className='text-center'>e90930f3-fa7b-4830-acba-e99d149b7721</h3>
+      </Container>
+      <Container className='d-flex justify-content-center'>
+        <Button className="mx-1">
+          Generate
+        </Button>
+      </Container>
+    </Container>
   );
 }
 
